302921a90274026548376dbe57163c4f98074436
orangain <orangain@gmail.com>
2017-03-29 20:32:56 +0900

    Show App's name, Kifu Notebook

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Kifu Notebook</h2>
         </div>
         <div className="App-body">
           <BoardSet />
