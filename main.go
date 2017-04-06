c53284de4aeea1d64609da6e21968b6c6d849fd5
orangain <orangain@gmail.com>
2017-04-06 23:04:42 +0900

    Add web server written in Go

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	host := "localhost"
+	port := 8888
+	addr := fmt.Sprintf("%s:%d", host, port)
+	url := fmt.Sprintf("http://%s/", addr)
+
+	http.Handle("/", http.FileServer(http.Dir("./build")))
+
+	// See: http://stackoverflow.com/questions/32738188/go-how-can-i-start-the-browser-after-the-server-started-listening
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	log.Println("Kifu Notebook is running at:", url)
+	open(url)
+
+	if err := http.Serve(l, nil); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+// See: http://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
