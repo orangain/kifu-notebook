c25f2fc0d866cc4ba30d38351317e93af8e5764c
orangain <orangain@gmail.com>
2017-04-19 13:07:30 +0900

    Include license comments in a minified code
    http://stackoverflow.com/questions/37899444/bsd-license-compliance-with-webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,7 +8,7 @@ var ManifestPlugin = require('webpack-manifest-plugin');
 var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 var paths = require('./paths');
 var getClientEnvironment = require('./env');
-
+var saveLicense = require('uglify-save-license');
 
 
 // Webpack uses `publicPath` to determine where the app is being served from.
@@ -85,7 +85,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -124,7 +124,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -165,9 +165,9 @@ module.exports = {
       // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
-  postcss: function() {
+  postcss: function () {
     return [
       autoprefixer({
         browsers: [
@@ -222,7 +222,7 @@ module.exports = {
         screw_ie8: true
       },
       output: {
-        comments: false,
+        comments: saveLicense,
         screw_ie8: true
       }
     }),
