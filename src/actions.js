a75a0b83de7643423761bc8614e251f28e813bea
orangain <orangain@gmail.com>
2017-04-20 15:32:51 +0900

    Debounce saving JKF

--- src/actions.js
+++ src/actions.js
@@ -27,34 +27,36 @@ export function fetchJKF() {
 }
 
 const clearMessageDebounce = debounce(5000);
+const storeJKFDebounce = debounce(500);
 
 export function storeJKF(jkf) {
-
   return (dispatch, getState) => {
-    dispatch(requestPutJKF());
-    const state = getState();
-    const body = JSON.stringify(state.jkf, null, '  ');
-    fetch('/jkf', { method: 'PUT', body: body })
-      .then((response) => {
-        if (response.ok) {
-          dispatch(receivePutJKF());
-        } else {
-          console.error(response);
+    storeJKFDebounce().then(() => {
+      dispatch(requestPutJKF());
+      const state = getState();
+      const body = JSON.stringify(state.jkf, null, '  ');
+      fetch('/jkf', { method: 'PUT', body: body })
+        .then((response) => {
+          if (response.ok) {
+            dispatch(receivePutJKF());
+          } else {
+            console.error(response);
+            console.log(body);  // to preserve data
+            dispatch(failPutJKF(response));
+          }
+          clearMessageDebounce().then(() => {
+            dispatch(clearMessage());
+          });
+        })
+        .catch((e) => {
+          dispatch(failPutJKF(e));
+          console.error(e);
           console.log(body);  // to preserve data
-          dispatch(failPutJKF(response));
-        }
-        clearMessageDebounce().then(() => {
-          dispatch(clearMessage());
+          clearMessageDebounce().then(() => {
+            dispatch(clearMessage());
+          });
         });
-      })
-      .catch((e) => {
-        dispatch(failPutJKF(e));
-        console.error(e);
-        console.log(body);  // to preserve data
-        clearMessageDebounce().then(() => {
-          dispatch(clearMessage());
-        });
-      });
+    });
   };
 }
 
