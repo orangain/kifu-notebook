d540c09ed5afd7885c125b6c9b9922139f982db2
orangain <orangain@gmail.com>
2017-04-24 09:31:37 +0900

    Use array-style pattern in redux-saga's takeEvery

--- src/sagas.js
+++ src/sagas.js
@@ -3,11 +3,7 @@ import { put, takeEvery } from 'redux-saga/effects';
 import { MOVE_PIECE, GO_BACK, GO_FORWARD, SCROLL_TO_CENTER } from './actions'
 
 export function* watchInputMove() {
-  yield [
-    takeEvery(MOVE_PIECE, scrollToCurrentNode),
-    takeEvery(GO_BACK, scrollToCurrentNode),
-    takeEvery(GO_FORWARD, scrollToCurrentNode),
-  ];
+  yield takeEvery([MOVE_PIECE, GO_BACK, GO_FORWARD], scrollToCurrentNode);
 }
 
 export function* scrollToCurrentNode() {
