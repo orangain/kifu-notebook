52953729b83b70bf940157bad21e764972865741
orangain <orangain@gmail.com>
2017-05-01 00:24:49 +0900

    Add config files for TypeScript

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -78,7 +78,7 @@ module.exports = {
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
     // https://github.com/facebookincubator/create-react-app/issues/290
-    extensions: ['.js', '.json', '.jsx', ''],
+    extensions: ['.js', '.json', '.jsx', '.ts', '.tsx', ''],
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
@@ -109,6 +109,7 @@ module.exports = {
         exclude: [
           /\.html$/,
           /\.(js|jsx)$/,
+          /\.(ts|tsx)$/,
           /\.css$/,
           /\.json$/,
           /\.svg$/
@@ -125,6 +126,11 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
 
+      },
+      // TypeScript
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'awesome-typescript-loader'
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
