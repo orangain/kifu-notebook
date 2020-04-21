a5f81328b69a285dc8e20b65a5f42b57de5b4c84
orangain <orangain@gmail.com>
2020-04-21 18:22:12 +0900

    Create react app from scratch

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
@@ -14,8 +14,11 @@ const store = createStore(reducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+    ,
+  </React.StrictMode>,
   document.getElementById("root")
 );
