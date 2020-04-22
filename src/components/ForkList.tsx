738d03451cb0836e9072205c62aee22b9161efff
orangain <orangain@gmail.com>
2020-04-23 00:43:27 +0900

    Refactor ForkList to be functional component

--- src/components/ForkList.tsx
+++ src/components/ForkList.tsx
@@ -1,54 +1,50 @@
-import * as React from "react";
-import { Component } from "react";
+import React from "react";
 
+import { Path, KifuTree } from "../models";
+import ForkListItem from "./ForkListItem";
+import ForkListJumpItem from "./ForkListJumpItem";
 import "./ForkList.css";
-import { KifuTreeNode, Path, KifuTree, JumpTarget } from "../models";
 
 interface ForkListProps {
   kifuTree: KifuTree;
   onClickPath: (path: Path) => void;
 }
-export default class ForkList extends Component<ForkListProps, {}> {
-  render() {
-    const { kifuTree, onClickPath } = this.props;
-    const { currentPath } = kifuTree;
-    const currentNode = kifuTree.getCurrentNode();
 
-    function renderList(): JSX.Element[] {
-      const forkList: JSX.Element[] = currentNode.children
-        .map((childNode: KifuTreeNode, i: number) => (
-          <li
-            key={childNode.readableKifu}
-            onClick={(e) => onClickPath(currentPath.concat([i]))}
-            className={childNode.isBad() ? "bad" : ""}
-          >
-            {childNode.readableKifu} <span className="comment">{childNode.comment}</span>
-          </li>
-        ))
-        .concat(
-          currentNode.jumpTargets.map((jumpTarget: JumpTarget) => (
-            <li
-              key={"jump-" + jumpTarget.readableKifu}
-              onClick={(e) => onClickPath(jumpTarget.path)}
-              className={jumpTarget.isBad() ? "bad" : ""}
-            >
-              ↪ {jumpTarget.readableKifu} <span className="comment">{jumpTarget.comment}</span>
-            </li>
-          ))
-        )
-        .toArray();
+const ForkList: React.FC<ForkListProps> = ({ kifuTree, onClickPath }) => {
+  const { currentPath } = kifuTree;
+  const currentNode = kifuTree.getCurrentNode();
 
-      if (forkList.length === 0) {
-        return [<li key="なし">なし</li>];
-      }
+  return (
+    <div className="ForkList">
+      <span>次の手</span>
+      <ul>
+        {currentNode.children.size + currentNode.jumpTargets.size > 0 ? (
+          [
+            ...currentNode.children
+              .toArray()
+              .map((childNode, i) => (
+                <ForkListItem
+                  key={childNode.readableKifu}
+                  childNode={childNode}
+                  onClick={() => onClickPath(currentPath.concat([i]))}
+                />
+              )),
+            ...currentNode.jumpTargets
+              .toArray()
+              .map((jumpTarget) => (
+                <ForkListJumpItem
+                  key={"jump-" + jumpTarget.readableKifu}
+                  jumpTarget={jumpTarget}
+                  onClick={() => onClickPath(jumpTarget.path)}
+                />
+              )),
+          ]
+        ) : (
+          <li key="なし">なし</li>
+        )}
+      </ul>
+    </div>
+  );
+};
 
-      return forkList;
-    }
-    return (
-      <div className="ForkList">
-        <span>次の手</span>
-        <ul>{renderList()}</ul>
-      </div>
-    );
-  }
-}
+export default ForkList;
---
+++ src/components/ForkListJumpItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { JumpTarget } from "../models";
+
+export type ForkListJumpItemProps = {
+  jumpTarget: JumpTarget;
+  onClick: React.MouseEventHandler;
+};
+
+const ForkListJumpItem: React.FC<ForkListJumpItemProps> = ({ jumpTarget, onClick }) => {
+  return (
+    <li onClick={onClick} className={jumpTarget.isBad() ? "bad" : ""}>
+      ↪ {jumpTarget.readableKifu} <span className="comment">{jumpTarget.comment}</span>
+    </li>
+  );
+};
+
+export default ForkListJumpItem;
