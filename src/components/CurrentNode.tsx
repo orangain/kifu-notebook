a17b36c815e2870c891159cac4213eafd8bbe0c7
orangain <orangain@gmail.com>
2020-04-23 20:31:51 +0900

    Refactor CurrentNode as function component

--- src/components/CurrentNode.tsx
+++ src/components/CurrentNode.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { Component } from "react";
-import "./CurrentNode.css";
 
 import ForkList from "./ForkList";
 import { Path, KifuTree } from "../models";
+import "./CurrentNode.css";
 
 export interface CurrentNodeStateProps {
   kifuTree: KifuTree;
@@ -15,62 +14,53 @@ export interface CurrentNodeDispatchProps {
   onBlurComments: () => void;
 }
 
-export default class CurrentNode extends Component<
-  CurrentNodeStateProps & CurrentNodeDispatchProps
-> {
-  onChangeComments(comment: string) {
-    this.props.onChangeComments(comment);
-  }
+const CurrentNode: React.FC<CurrentNodeStateProps & CurrentNodeDispatchProps> = ({
+  kifuTree,
+  onClickPath,
+  onChangeComments,
+  onBlurComments,
+}) => {
+  const currentNode = kifuTree.getCurrentNode();
+  const previousPath = kifuTree.getPreviousPath();
+  const nextPath = kifuTree.getNextPath();
+  const previousForkPath = kifuTree.getPreviousForkPath();
+  const nextForkPath = kifuTree.getNextForkPath();
+  const comment = currentNode.comment;
 
-  render() {
-    const { kifuTree } = this.props;
-    const currentNode = kifuTree.getCurrentNode();
-    const previousPath = kifuTree.getPreviousPath();
-    const nextPath = kifuTree.getNextPath();
-    const previousForkPath = kifuTree.getPreviousForkPath();
-    const nextForkPath = kifuTree.getNextForkPath();
-    const comment = currentNode.comment;
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          {currentNode.tesuu === 0 ? null : currentNode.tesuu + "手目"} {currentNode.readableKifu}
-        </div>
-        <textarea
-          rows={10}
-          className="comment"
-          placeholder="ここに現在の手についてコメントを書けます。"
-          onChange={(e) => {
-            this.onChangeComments(e.target.value);
-          }}
-          onBlur={(e) => {
-            this.props.onBlurComments();
-          }}
-          value={comment}
-        ></textarea>
-        <div className="buttons">
-          <button
-            onClick={(e) => this.props.onClickPath(previousForkPath)}
-            title="1つ前の分岐に戻る"
-          >
-            &laquo;
-          </button>
-          <button onClick={(e) => this.props.onClickPath(previousPath)} title="1手戻る">
-            &lt;
-          </button>
-          <button onClick={(e) => this.props.onClickPath(nextPath)} title="1手進む">
-            &gt;
-          </button>
-          <button onClick={(e) => this.props.onClickPath(nextForkPath)} title="1つ先の分岐に進む">
-            &raquo;
-          </button>
-        </div>
-        <ForkList
-          currentNode={currentNode}
-          currentPath={kifuTree.currentPath}
-          onClickPath={this.props.onClickPath}
-        />
+        {currentNode.tesuu === 0 ? null : currentNode.tesuu + "手目"} {currentNode.readableKifu}
       </div>
-    );
-  }
-}
+      <textarea
+        rows={10}
+        className="comment"
+        placeholder="ここに現在の手についてコメントを書けます。"
+        onChange={(e) => onChangeComments(e.target.value)}
+        onBlur={onBlurComments}
+        value={comment}
+      ></textarea>
+      <div className="buttons">
+        <button onClick={() => onClickPath(previousForkPath)} title="1つ前の分岐に戻る">
+          &laquo;
+        </button>
+        <button onClick={() => onClickPath(previousPath)} title="1手戻る">
+          &lt;
+        </button>
+        <button onClick={() => onClickPath(nextPath)} title="1手進む">
+          &gt;
+        </button>
+        <button onClick={() => onClickPath(nextForkPath)} title="1つ先の分岐に進む">
+          &raquo;
+        </button>
+      </div>
+      <ForkList
+        currentNode={currentNode}
+        currentPath={kifuTree.currentPath}
+        onClickPath={onClickPath}
+      />
+    </div>
+  );
+};
+
+export default CurrentNode;
