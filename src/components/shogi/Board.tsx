76b6880038639d054f01b0ed5f7562b34093e46a
orangain <orangain@gmail.com>
2020-04-25 21:21:16 +0900

    Show number header in Board

--- src/components/shogi/Board.tsx
+++ src/components/shogi/Board.tsx
@@ -11,11 +11,24 @@ export type BoardProps = {
   onInputMove: (move: IMoveMoveFormat) => void;
 };
 
+const kansuuji = ["一", "二", "三", "四", "五", "六", "七", "八", "九"];
+
+const range = (n: number): number[] => [...Array(n)].map((_, i) => i);
+
 export const Board: React.FC<BoardProps> = ({ board, lastMovedPlace, reversed, onInputMove }) => {
   return (
     <div className={`board ${reversed ? "reversed" : ""}`}>
-      {board.map((row, i) =>
-        row.map((piece, j) => {
+      <div key="origin" className="header-square" />
+      {range(9).map((i) => (
+        <div key={`row-${i}`} className="header-square row">
+          {kansuuji[i]}
+        </div>
+      ))}
+      {board.map((row, i) => [
+        <div key={`column-${i}`} className="header-square column">
+          {i + 1}
+        </div>,
+        ...row.map((piece, j) => {
           const x = i + 1;
           const y = j + 1;
           const isActive =
@@ -30,8 +43,8 @@ export const Board: React.FC<BoardProps> = ({ board, lastMovedPlace, reversed, o
               onInputMove={onInputMove}
             />
           );
-        })
-      )}
+        }),
+      ])}
     </div>
   );
 };
