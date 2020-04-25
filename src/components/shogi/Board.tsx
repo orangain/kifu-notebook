a194c7613ad4c9acc4a7c1fb90aefe15fe6e2c5b
orangain <orangain@gmail.com>
2020-04-25 21:34:06 +0900

    Implement reversing board by script not by CSS
    Required to reverse headers properly

--- src/components/shogi/Board.tsx
+++ src/components/shogi/Board.tsx
@@ -11,7 +11,7 @@ export type BoardProps = {
   onInputMove: (move: IMoveMoveFormat) => void;
 };
 
-const kansuuji = ["一", "二", "三", "四", "五", "六", "七", "八", "九"];
+const kansuuji = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
 
 const range = (n: number): number[] => [...Array(n)].map((_, i) => i);
 
@@ -19,32 +19,42 @@ export const Board: React.FC<BoardProps> = ({ board, lastMovedPlace, reversed, o
   return (
     <div className={`board ${reversed ? "reversed" : ""}`}>
       <div key="origin" className="header-square" />
-      {range(9).map((i) => (
-        <div key={`row-${i}`} className="header-square row">
-          {kansuuji[i]}
-        </div>
-      ))}
-      {board.map((row, i) => [
-        <div key={`column-${i}`} className="header-square column">
-          {i + 1}
-        </div>,
-        ...row.map((piece, j) => {
-          const x = i + 1;
-          const y = j + 1;
-          const isActive =
-            lastMovedPlace != null && lastMovedPlace.x === x && lastMovedPlace.y === y;
-          return (
-            <BoardSquare
-              key={`${x}-${y}`}
-              place={{ x, y }}
-              piece={piece}
-              isActive={isActive}
-              reversed={reversed}
-              onInputMove={onInputMove}
-            />
-          );
-        }),
-      ])}
+      {range(9).map((i) => {
+        const normalizedI = !reversed ? i : 8 - i;
+        const y = normalizedI + 1;
+        return (
+          <div key={`row-${y}`} className="header-square row">
+            {kansuuji[y]}
+          </div>
+        );
+      })}
+      {board.map((_, i) => {
+        const normalizedI = !reversed ? i : 8 - i;
+        const column = board[normalizedI];
+        const x = normalizedI + 1;
+        return [
+          <div key={`column-${x}`} className="header-square column">
+            {x}
+          </div>,
+          ...column.map((_, j) => {
+            const normalizedJ = !reversed ? j : 8 - j;
+            const piece = column[normalizedJ];
+            const y = normalizedJ + 1;
+            const isActive =
+              lastMovedPlace != null && lastMovedPlace.x === x && lastMovedPlace.y === y;
+            return (
+              <BoardSquare
+                key={`${x}-${y}`}
+                place={{ x, y }}
+                piece={piece}
+                isActive={isActive}
+                reversed={reversed}
+                onInputMove={onInputMove}
+              />
+            );
+          }),
+        ];
+      })}
     </div>
   );
 };
